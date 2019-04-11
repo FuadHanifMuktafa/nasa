b550569b4b75ba990128864ce40d28af16d2b2de
FuadHanifMuktafa <45312792+FuadHanifMuktafa@users.noreply.github.com>
2019-04-11 18:28:03 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=01BWHiPyYyebE7dnZZE84ag2vTUseciHaJDJDD0A";
         
         $("#loadImage").click(function(){
             $.ajax({
